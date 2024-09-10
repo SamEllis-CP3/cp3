a1c1980645c941b2cfc0bd8a284d7c4a80ae9fa7
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-09-10 17:48:17 +0000

    added a number even or odd checker

--- madlib.py
+++ madlib.py
@@ -7,7 +7,11 @@ print("new")
 print("this is the second test")
 
 print("This is a test from the code space side")
-num = random.randint(0,5)
+num = random.randint(0,25)
+if (num % 2) == 1:
+    print("num is even")
+else:
+    print("Num is odd")
 while True:
     g = int(input("pick a number: "))
     if g == num:
