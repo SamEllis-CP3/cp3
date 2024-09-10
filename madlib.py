4b34f16f8b79ecd68bb250772e6a18d419c3aa49
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-09-10 11:46:31 -0600

    added number guesser

--- madlib.py
+++ madlib.py
@@ -1,3 +1,4 @@
+import random
 print("This is a Test")
 
 
@@ -6,3 +7,11 @@ print("new")
 print("this is the second test")
 
 print("This is a test from the code space side")
+num = random.randint(0,5)
+while True:
+    g = int(input("pick a number: "))
+    if g == num:
+        print("you won")
+        break
+    else:
+        print("Your wrong try again")
