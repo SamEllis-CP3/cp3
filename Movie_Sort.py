ee30f0771c3a488378872ddbe311d385799c22e1
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-10-08 03:20:59 +0000

    fixed naming issues on movie code

--- Movie_Sort.py
+++ Movie_Sort.py
@@ -1,60 +1,58 @@
-
-
 class Movie:
-    def __init__(self, name, year, director, rating, genrea, cast):
+    def __init__(self, name, year, director, rating, genre, cast):
         self.name = name
         self.year = year
         self.director = director
         self.rating = rating
-        self.genrea = genrea
+        self.genre = genre
         self.cast = cast
 
     def __str__(self):
-        return f"Name: {self.name}\nYear: {self.year}\nDirector: {self.director}\nRating: {self.rating}\nGenrea: {self.genrea}\nCast: {self.cast}"
-    
+        return f"Name: {self.name}\nYear: {self.year}\nDirector: {self.director}\nRating: {self.rating}\nGenre: {self.genre}\nCast: {', '.join(self.cast)}"
+
     def sort_Alpha(self, movie_list):
         Aplha_sort = []
         while movie_list:
             low_movie = movie_list[0]
             for movie in movie_list:
-                if movie_list.name < low_movie.name:
+                if movie.name < low_movie.name:
                     low_movie = movie
-                Aplha_sort.append(movie_list)
-                movie_list.remove(low_movie)
-            return Aplha_sort
-        
+            Aplha_sort.append(low_movie)
+            movie_list.remove(low_movie)
+        return Aplha_sort
+
     def sort_chrono(self, movie_list):
         chrono_sort = []
         while movie_list:
             low_movie = movie_list[0]
             for movie in movie_list:
-                if movie_list.year < low_movie.year:
+                if movie.year < low_movie.year:
                     low_movie = movie
-                chrono_sort.append(movie_list)
-                movie_list.remove(low_movie)
-            return chrono_sort
-    
+            chrono_sort.append(low_movie)
+            movie_list.remove(low_movie)
+        return chrono_sort
+
     def genre_sort(self, movie_list, genre):
         newmovie_list = []
         for movie in movie_list:
-            if movie_list.genre.lower() == genre.lower():
+            if movie.genre.lower() == genre.lower():
                 newmovie_list.append(movie)
         return newmovie_list
-    
+
     def director_sort(self, movie_list, director):
         newmovie_list = []
         for movie in movie_list:
-            if movie_list.director.lower() == director.lower():
+            if movie.director.lower() == director.lower():
                 newmovie_list.append(movie)
         return newmovie_list
-    
-    def director_sort(self, movie_list, actor):
+
+    def cast_sort(self, movie_list, actor):
         newmovie_list = []
         for movie in movie_list:
-            if movie_list.actor.lower() == actor.lower():
+            if actor.lower() in [cast_member.lower() for cast_member in movie.cast]:
                 newmovie_list.append(movie)
         return newmovie_list
-    
+
     def movie_menu(self):
         print("\n***** Movie Sorting Menu *****")
         print("1. View Movies Sorted Alphabetically")
@@ -65,8 +63,49 @@ class Movie:
         print("6. Exit")
         print("*****************************")
 
-    def run(self, movie_list):
-        
+    def runmovie(self, movie_list):
+        while True:
+            self.movie_menu()
+            ch = int(input("Please Choose 1-6: "))
+
+            if ch == 1:
+                print("\nMovies sorted alphabetically:")
+                sorted_movies = self.sort_Alpha(movie_list.copy())
+                for movie in sorted_movies:
+                    print(movie)
+
+            elif ch == 2:
+                print("\nMovies sorted chronologically:")
+                sorted_movies = self.sort_chrono(movie_list.copy())
+                for movie in sorted_movies:
+                    print(movie)
+
+            elif ch == 3:
+                genre = input("Enter the genre to search for: ")
+                print(f"\nMovies in the '{genre}' genre:")
+                found_movies = self.genre_sort(movie_list, genre)
+                for movie in found_movies:
+                    print(movie)
+
+            elif ch == 4:
+                director = input("Enter the director's name to search for: ")
+                print(f"\nMovies directed by '{director}':")
+                found_movies = self.director_sort(movie_list, director)
+                for movie in found_movies:
+                    print(movie)
+
+            elif ch == 5:
+                actor = input("Enter the actor's name to search for: ")
+                print(f"\nMovies with '{actor}' in the cast:")
+                found_movies = self.cast_sort(movie_list, actor)
+                for movie in found_movies:
+                    print(movie)
+
+            elif ch == 6:
+                print("Leaving Program")
+                break
+            else:
+                print("Please try again.")
 
 movie_list = [
     Movie("The Shawshank Redemption", 1994, "Frank Darabont", "R", "Drama", ["Tim Robbins", "Morgan Freeman"]),
@@ -90,3 +129,7 @@ movie_list = [
     Movie("The Lion King", 1994, "Roger Allers, Rob Minkoff", "G", "Animation", ["Matthew Broderick", "Jeremy Irons", "James Earl Jones"]),
     Movie("Eternal Sunshine of the Spotless Mind", 2004, "Michel Gondry", "R", "Romance", ["Jim Carrey", "Kate Winslet", "Kirsten Dunst"]),
 ]
+
+startmovie = Movie("", 0, "", "", "", [])
+
+startmovie.runmovie(movie_list)
