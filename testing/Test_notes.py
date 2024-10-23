4d0c852902e4976199d6845a89169f96d623e9e7
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-10-23 11:43:21 -0600

    added testing thinggy

--- testing/Test_notes.py
+++ testing/Test_notes.py
@@ -4,3 +4,9 @@ def test_get_name():
     name = testanimal.get_name()
     assert name == "Bob"
 
+def test_get_namep2():
+    testanimal = animal("Jarrod", "Frog", 2, "F", "Legendary")
+    name = testanimal.get_name()
+    assert name == "Jarrod"
+
+
