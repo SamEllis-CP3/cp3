fe1f203d856da228841afaa3299eb656b4dfe8e5
SamEllis-CP3 <samuel.ellis@ucas-edu.net>
2024-10-24 11:43:06 -0600

    added_desert_test

--- Dessert_Shop.py
+++ Dessert_Shop.py
@@ -1,35 +1,57 @@
 class DessertItem:
     def __init__(self, name):
         self.name = name
+    def get_name(self):
+        return self.name
+    
     
 class Candy(DessertItem):
     def __init__(self, name,candy_weight, price_per_pound):
         super().__init__(name)
         self.candy_weight = candy_weight
         self.price_per_pound = price_per_pound
-    def get_name(self):
-        return self.name
+    
+    def get_cost(self):
+        return self.price_per_pound
+    
+    def get_weight(self):
+        return self.candy_weight
+    
 
 class Cookie(DessertItem):
     def __init__(self, name, quantity, price_per_dozen):
         super().__init__(name)
         self.quantity = quantity
         self.price_per_dozen = price_per_dozen
-    def get_name(self):
-        return self.name
+
+    def get_cost(self):
+        return self.price_per_dozen
+    
+    def get_amount(self):
+        return self.quantity
+    
     
 class IceCream(DessertItem):
     def __init__(self, name, scoop_count, price_per_scoop):
         super().__init__(name)
         self.scoop_count = scoop_count
         self.price_per_scoop = price_per_scoop
-    def get_name(self):
-        return self.name
+    
+    def get_scoop(self):
+        return self.scoop_count
+    
+    def get_cost(self):
+        return self.price_per_scoop
 
 class Sundae(IceCream):
     def __init__(self, name, scoop_count, price_per_scoop, topping_name, topping_price):
         super().__init__(name, scoop_count, price_per_scoop)
         self.topping_name = topping_name
         self.topping_price = topping_price
-    def get_name(self):
-        return self.name
+
+    def get_topping(self):
+        return self.topping_name
+    
+    def get_topping_price(self):
+        return self.topping_price
+    
--- testing/Test_Dessert.py
+++ testing/Test_Dessert.py
@@ -1,6 +1,80 @@
 from Dessert_Shop import *
 
 def test_candy():
-    testweight = Candy("Jolly Ranchers", 2, 6.50)
-    weight = testweight.get_name()
-    assert weight == 2
+    testcandy_1 = Candy("Jolly Ranchers", 2, 6.50)
+    testcandy_2 = Candy("M&M", 1, 7.25)
+    testcandy_3 = Candy("Jelly Beans", 27, 3.50)
+    
+    assert testcandy_1.get_weight() == 2
+    assert testcandy_1.get_cost() == 6.50
+    assert testcandy_1.get_name() == "Jolly Ranchers"
+
+    assert testcandy_2.get_weight() == 1
+    assert testcandy_2.get_cost() == 7.25
+    assert testcandy_2.get_name() == "M&M"
+
+    assert testcandy_3.get_weight() == 27
+    assert testcandy_3.get_cost() == 3.50
+    assert testcandy_3.get_name() == "Jelly Beans"
+
+
+def test_cookie():
+    test_cookies_1 = Cookie("choclate chip", 36, 3.75)
+    test_cookies_2 = Cookie("Snicker doodle", 24, 5.50)
+    test_cookies_3 = Cookie("Sugar", 48, 3.50)
+    
+    assert test_cookies_1.get_amount() == 36
+    assert test_cookies_1.get_cost() == 3.75
+    assert test_cookies_1.get_name() == "choclate chip"
+
+    assert test_cookies_2.get_amount() == 24
+    assert test_cookies_2.get_cost() == 5.50
+    assert test_cookies_2.get_name() == "Snicker doodle"
+
+    assert test_cookies_3.get_amount() == 48
+    assert test_cookies_3.get_cost() == 3.50
+    assert test_cookies_3.get_name() == "Sugar"
+
+
+def test_ice_cream():
+    test_icecream_1 = IceCream("Starwberry", 2, 2.50)
+    test_icecream_2 = IceCream("Chocolate", 3, 1.75)
+    test_icecream_3 = IceCream("Pecan", 1, 3.50)
+    
+    assert test_icecream_1.get_cost() == 2.50
+    assert test_icecream_1.get_scoop() == 2
+    assert test_icecream_1.get_name() == "Starwberry"
+
+    assert test_icecream_2.get_cost() == 1.75
+    assert test_icecream_2.get_scoop() == 3
+    assert test_icecream_2.get_name() == "Chocolate"
+
+    assert test_icecream_3.get_cost() == 3.50
+    assert test_icecream_3.get_scoop() == 1
+    assert test_icecream_3.get_name() == "Pecan"
+
+def test_sundae():
+    test_sun1 = Sundae("Starwberry", 2, 2.50, "sprinkles", 0.25)
+    test_sun2 = Sundae("Chocolate", 3, 1.75, "Fudge", 0.75)
+    test_sun3 = Sundae("Pecan", 1, 3.50, "Nuts", 0.50)
+    
+    assert test_sun1.get_cost() == 2.50
+    assert test_sun1.get_scoop() == 2
+    assert test_sun1.get_name() == "Starwberry"
+    assert test_sun1.get_topping() == "sprinkles"
+    assert test_sun1.get_topping_price() == 0.25
+
+    assert test_sun2.get_cost() == 1.75
+    assert test_sun2.get_scoop() == 3
+    assert test_sun2.get_name() == "Chocolate"
+    assert test_sun2.get_topping() == "Fudge"
+    assert test_sun2.get_topping_price() == 0.75
+
+    assert test_sun3.get_cost() == 3.50
+    assert test_sun3.get_scoop() == 1
+    assert test_sun3.get_name() == "Pecan"
+    assert test_sun3.get_topping() == "Nuts"
+    assert test_sun3.get_topping_price() == 0.50
+
+    
+
